9cd3e76dab91a32c26292c87dc40bdb3505ce4ba
Leonid Verhovskij <l.verhovskij@gmail.com>
2016-09-19 17:05:20 +0200

    implement extension.json for extension loading

--- EchoConnector.setup.php
+++ EchoConnector.setup.php
@@ -1,13 +1,27 @@
 <?php
 
-$wgMessagesDirs['EchoConnector'] = __DIR__ . '/i18n';
-$wgMessagesDirs['BSFoundation'] = __DIR__ . '/../../BlueSpiceFoundation/i18n';
-$wgAutoloadClasses['EchoConnectorHooks'] = __DIR__."/includes/EchoConnectorHooks.php";
-$wgAutoloadClasses['BSEchoNotificationHandler'] = __DIR__."/includes/EchoNotificationHandler.php";
-$wgAutoloadClasses['BsNotificationsFormatter'] = __DIR__."/includes/NotificationsFormatter.php";
-$wgAutoloadClasses['BsEchoEmailSingle'] = __DIR__."/includes/EchoEmailSingle.php";
-$wgAutoloadClasses['BsEchoTextEmailFormatter'] = __DIR__."/includes/EchoTextEmailFormatter.php";
-$wgAutoloadClasses['BsEchoTextEmailDecorator'] = __DIR__."/includes/EchoTextEmailDecorator.php";
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension("BlueSpiceEchoConnector");
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['BlueSpiceEchoConnector'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for BlueSpiceEchoConnector extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the BlueSpiceEchoConnector extension requires MediaWiki 1.25+' );
+}
 
-$wgHooks['BeforeNotificationsInit'][] = "EchoConnectorHooks::onBeforeNotificationsInit";
-$wgHooks['EchoGetDefaultNotifiedUsers'][] = "EchoConnectorHooks::onEchoGetDefaultNotifiedUsers";
+
+$echoRessourcePackages = array(
+	'ext.echo.base', 'ext.echo.overlay', 'ext.echo.overlay.init',
+	'ext.echo.special', 'ext.echo.alert', 'ext.echo.badge'
+);
+
+foreach( $echoRessourcePackages as $package ) {
+	$wgResourceModules[$package]['remoteExtPath'] = 'BlueSpiceDistribution/Echo/modules';
+}
+
+unset( $echoRessourcePackages );
