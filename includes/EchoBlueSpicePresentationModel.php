6c02e42f8974df679cecbebcc493f346fb23e855
pwirth <wirth@hallowelt.biz>
2016-10-06 15:59:23 +0200

    Added hacky generic PresentationModel

---
+++ includes/EchoBlueSpicePresentationModel.php
@@ -0,0 +1,51 @@
+<?php
+
+class EchoBlueSpicePresentationModel extends EchoEventPresentationModel {
+	public function getIconType() {
+		global $wgEchoNotifications;
+		return $this->getFormatter( $wgEchoNotifications[$this->type] )->icon;
+	}
+
+	public function getPrimaryLink() {
+		return $this->event->getTitle()
+			? array(
+				'url' => $this->event->getTitle()->getFullURL(),
+				'label' => $this->event->getTitle()->getText()
+			)
+			: false
+		;
+	}
+
+	public function getHeaderMessageKey() {
+		global $wgEchoNotifications;
+		return $wgEchoNotifications[$this->type]['email-subject-message'];
+	}
+
+	public function getHeaderMessage() {
+		$oMsg = $this->msg( $this->getHeaderMessageKey() );
+		$oFormatter = $this->getFormatter();
+
+		$aParams = $GLOBALS['wgEchoNotifications'][$this->type]
+			['email-subject-params'];
+		if( empty($aParams) ) {
+			return $oMsg;
+		}
+
+		foreach($aParams as $param ) {
+			$oFormatter->processParam(
+				$this->event,
+				$param,
+				$oMsg,
+				$this->event->getAgent()
+			);
+		}
+		return $oMsg;
+	}
+
+	public function getFormatter() {
+		global $wgEchoNotifications;
+		return new BsNotificationsFormatter(
+			$wgEchoNotifications[$this->type]
+		);
+	}
+}
