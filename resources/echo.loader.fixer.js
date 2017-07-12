fcb4fda4723af8b6ff0dec995e83d79b63150a96
Dejan Savuljesku <d.savuljesku@gmail.com>
2017-07-12 11:16:27 +0200

    Fix for Echo not loading message- and alert Widgets
    See more in the ticket
    
    ERM: #6326

---
+++ resources/echo.loader.fixer.js
@@ -0,0 +1,13 @@
+(function( mw ) {
+	mw.echo.ui.messageWidget = {
+		fetchUnreadCountFromApi: function() {
+			return 0;
+		}
+	}
+	mw.echo.ui.alertWidget = {
+		fetchUnreadCountFromApi: function() {
+			return 0;
+		}
+	}
+})( mediaWiki );
+
