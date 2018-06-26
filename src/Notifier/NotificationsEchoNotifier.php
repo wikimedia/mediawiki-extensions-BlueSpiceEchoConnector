cf5255eb139a8aa9dd049ec99e501836d09d375b
Dejan Savuljesku <d.savuljesku@gmail.com>
2018-06-26 15:31:25 +0200

    Do not require two params for registering notif category
    Change-Id: I6f1c1e3261a5e8f9f2be4bb7cacbfeca1acd9b04

--- src/Notifier/NotificationsEchoNotifier.php
+++ src/Notifier/NotificationsEchoNotifier.php
@@ -179,7 +179,7 @@ class NotificationsEchoNotifier implements \BlueSpice\INotifier {
 	 * @param string $key
 	 * @param array $params
 	*/
-	public function registerNotificationCategory( $key, $params ) {
+	public function registerNotificationCategory( $key, $params = [] ) {
 		$this->echoNotificationCategories[$key] = $params;
 	}
 
