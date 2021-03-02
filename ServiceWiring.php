23e1569483fd60fbd25957444082a3c55bc4e6ea
d.savuljesku <savuljesku@hallowelt.com>
2021-03-02 11:00:51 +0100

    User MaintenanceUser to retrieve users watching a page
    [3.2]
    
    ERM 23036
    
    Change-Id: I4e041807772d307a6c5252aa5fc686fe51799a6d

--- ServiceWiring.php
+++ ServiceWiring.php
@@ -1,9 +1,15 @@
 <?php
 
 use BlueSpice\EchoConnector\UserLocator;
+use MediaWiki\MediaWikiServices;
 
 return [
-	'BSEchoConnectorUserLocator' => function ( \MediaWiki\MediaWikiServices $services ) {
-		return new UserLocator( $services->getDBLoadBalancer(), RequestContext::getMain() );
+	'BSEchoConnectorUserLocator' => function ( MediaWikiServices $services ) {
+		$context = new DerivativeContext( RequestContext::getMain() );
+		$context->setUser(
+			$services->getService( 'BSUtilityFactory' )->getMaintenanceUser()->getUser()
+		);
+
+		return new UserLocator( $services->getDBLoadBalancer(), $context );
 	}
 ];
