a2bce927a9efc6e8bfe61323f273e70a7c8cf9b6
pwirth <wirth@hallowelt.biz>
2021-07-05 11:39:01 +0200

    Fix notification jobs not having correct namespace/Text pairs in database
    ERM:24241
    [4.0.x]
    
    Change-Id: Ieda899792752fb035d43e5e4bffb00389f825347

--- src/Job/SendNotification.php
+++ src/Job/SendNotification.php
@@ -2,6 +2,8 @@
 
 namespace BlueSpice\EchoConnector\Job;
 
+use Title;
+
 class SendNotification extends \Job {
 
 	/**
@@ -10,7 +12,16 @@ class SendNotification extends \Job {
 	 * @param array $params
 	 */
 	public function __construct( $title, $params ) {
-		parent::__construct( 'sendNotification', $title, $params );
+		if ( $title instanceof Title ) {
+			// parent::__construct Backwards compatibility for old signature
+			// ($command, $title, $params) seems to have an issue retrieving the
+			// actual correct data and ends up writing -1|FullText into the DB
+			// ERM:24241
+			$this->title = $title;
+			$params['namespace'] = $title->getNamespace();
+			$params['title'] = $title->getDBkey();
+		}
+		parent::__construct( 'sendNotification', $params );
 	}
 
 	public function run() {
