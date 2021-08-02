2f35fde15d4d45acf9cba0b02f06bde27b6ac1f3
d.savuljesku <savuljesku@hallowelt.com>
2021-08-02 09:40:48 +0200

    Make sure we dont put whole Title and User objects to Job params
    [3.2.x]
    
    ERM 21814
    
    Change-Id: Idda91bfe57b2fda41150ce52158b0105ea3fde48

--- src/Job/SendNotification.php
+++ src/Job/SendNotification.php
@@ -3,12 +3,13 @@
 namespace BlueSpice\EchoConnector\Job;
 
 use Title;
+use User;
 
 class SendNotification extends \Job {
 
 	/**
 	 *
-	 * @param \Title $title
+	 * @param Title $title
 	 * @param array $params
 	 */
 	public function __construct( $title, $params ) {
@@ -21,10 +22,36 @@ class SendNotification extends \Job {
 			$params['namespace'] = $title->getNamespace();
 			$params['title'] = $title->getDBkey();
 		}
-		parent::__construct( 'sendNotification', $params );
+		parent::__construct( 'sendNotification', $this->compressParams( $params ) );
 	}
 
 	public function run() {
+		$this->expandParams();
 		\EchoEvent::create( $this->params );
+	}
+
+	/**
+	 * @param array $params
+	 * @return array
+	 */
+	private function compressParams( $params ) {
+		if ( isset( $params['title'] ) && $params['title'] instanceof Title ) {
+			$params['title'] = $params['title']->getArticleID();
+		}
+
+		if ( isset( $params['agent'] ) && $params['agent'] instanceof User ) {
+			$params['agent'] = $params['agent']->getId();
+		}
+		return $params;
+	}
+
+	private function expandParams() {
+		if ( isset( $this->params['title'] ) && is_int( $this->params['title'] ) ) {
+			$this->params['title'] = Title::newFromID( $this->params['title'] );
+		}
+
+		if ( isset( $this->params['agent'] ) && is_int( $this->params['agent'] ) ) {
+			$this->params['agent'] = User::newFromId( $this->params['agent'] );
+		}
 	}
 }
