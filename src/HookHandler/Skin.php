a42ba159b5f128dc2c24ff69e8a033808c11157e
pwirth <wirth@hallowelt.biz>
2021-10-26 16:21:40 +0200

    Add attention indicator to personal menu
    ERM:25779
    [4.1]
    
    Change-Id: Ia037ba9488745c2309a19bd7adfa55b440065b2f

---
+++ src/HookHandler/Skin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace BlueSpice\EchoConnector\HookHandler;
+
+use MediaWiki\Hook\PersonalUrlsHook;
+use SkinTemplate;
+use Title;
+
+class Skin implements PersonalUrlsHook {
+
+	/**
+	 * @param array &$personal_urls
+	 * @param Title &$title
+	 * @param SkinTemplate $skin
+	 * @return void
+	 */
+	public function onPersonalUrls( &$personal_urls, &$title, $skin ): void {
+		if ( isset( $personal_urls['notifications-alert'] ) ) {
+			if ( !isset( $personal_urls['notifications-alert']['data'] ) ) {
+				$personal_urls['notifications-alert']['data'] = [];
+			}
+			$personal_urls['notifications-alert']['data']['attentionindicator']
+				= 'notifications-alert';
+		}
+		if ( isset( $personal_urls['notifications-notice'] ) ) {
+			if ( !isset( $personal_urls['notifications-notice']['data'] ) ) {
+				$personal_urls['notifications-notice']['data'] = [];
+			}
+			$personal_urls['notifications-notice']['data']['attentionindicator']
+				= 'notifications-notice';
+		}
+	}
+
+}
