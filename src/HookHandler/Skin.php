1cffabd2882d35398834edb4b1231f7bbe9cb7a3
pwirth <wirth@hallowelt.biz>
2021-11-10 14:13:00 +0100

    Replace notices and alerts personal urls with notifications
    ERM:26399
    [4.x]
    
    Change-Id: Ic168d5c3815b1db8e5959d7f485fd0106c795027

--- src/HookHandler/Skin.php
+++ src/HookHandler/Skin.php
@@ -3,7 +3,9 @@
 namespace BlueSpice\EchoConnector\HookHandler;
 
 use MediaWiki\Hook\PersonalUrlsHook;
+use MWEchoNotifUser;
 use SkinTemplate;
+use SpecialPage;
 use Title;
 
 class Skin implements PersonalUrlsHook {
@@ -31,6 +33,31 @@ class Skin implements PersonalUrlsHook {
 				= 'notifications-notice';
 			$personal_urls['notifications-notice']['position'] = 110;
 		}
+
+		if ( $skin->getSkinName() !== 'bluespicediscovery' ) {
+			return;
+		}
+		if ( isset( $personal_urls['notifications-alert'] ) ) {
+			unset( $personal_urls['notifications-alert'] );
+		}
+		if ( isset( $personal_urls['notifications-notice'] ) ) {
+			unset( $personal_urls['notifications-notice'] );
+		}
+		$notifUser = MWEchoNotifUser::newFromUser( $skin->getUser() );
+		$count = $notifUser->getAlertCount() + $notifUser->getMessageCount();
+		$url = SpecialPage::getTitleFor( 'Notifications' )->getLocalURL();
+		$msg = $skin->msg( 'bs-echoconnector-personalurl-notifications' )->params( $count );
+		$personal_urls['bsec-notifications'] = [
+			'href' => $url,
+			'text' => $msg->text(),
+			'active' => $url == $title->getLocalURL(),
+			'data' => [
+				'counter-num' => $count,
+				'counter-text' => $msg->text(),
+				'attentionindicator' => 'notifications',
+			],
+			'position' => 100,
+		];
 	}
 
 }
