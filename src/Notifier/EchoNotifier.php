4ff1301997e65ed013dcd59540af3b37c3172e7f
rvogel <vogel@hallowelt.biz>
2023-05-04 15:52:10 +0200

    Fix call to undefined method
    Extension:Echo@REL1_39 does not have
    `MWEchoEventLogging::logSchemaEcho`anymore.
    
    ERM32410
    
    Change-Id: If5aaee4e44e462fc7df341d05257fe440aee06e1

--- src/Notifier/EchoNotifier.php
+++ src/Notifier/EchoNotifier.php
@@ -95,7 +95,7 @@ class EchoNotifier extends \EchoNotifier {
 				$bundleHash = md5( $bundleString );
 			}
 
-			\MWEchoEventLogging::logSchemaEcho( $user, $event, 'email' );
+			\MWEchoEventLogging::logSchemaEchoMail( $user, 'single' );
 
 			MediaWikiServices::getInstance()->getHookContainer()->run(
 				'BlueSpiceEchoConnectorNotifyBeforeSend',
